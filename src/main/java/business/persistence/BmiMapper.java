b68b98a69ce8e216d998ae83f2aaeafb74b1cd20
Mathias Filtenborg Hansen <didas96@gmail.com>
2021-04-13 14:09:45 +0200

    til og med anden lektion tirsdag

---
+++ src/main/java/business/persistence/BmiMapper.java
@@ -0,0 +1,96 @@
+package business.persistence;
+
+import business.exceptions.UserException;
+
+import java.sql.*;
+import java.util.List;
+
+public class BmiMapper {
+    private Database database;
+
+    public BmiMapper(Database database) {
+        this.database = database;
+    }
+
+    public void insertBmiEntry(double bmi,
+                               double height,
+                               double weight,
+                               String category,
+                               String gender,
+                               int sport_id,
+                               int user_id,
+                               List<Integer> hobbyList) throws UserException{
+
+        try (Connection connection = database.connect())
+        {
+            String sql =
+                    "INSERT INTO `bmi`.`bmi_entry` " +
+                    "("  +
+                    "`height`," +
+                    "`weight`," +
+                    "`category`," +
+                    "`bmi`," +
+                    "`gender`," +
+                    "`sport_id`," +
+                    "`user_id`) " +
+                    "VALUES (?, ?, ?, ?, ?, ?, ?);";
+
+            try (PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS))
+            {
+                ps.setDouble(1, height);
+                ps.setDouble(2, weight);
+                ps.setString(3, category);
+                ps.setDouble(4, bmi);
+                ps.setString(5, gender);
+                ps.setInt(6, sport_id);
+                ps.setInt(7, user_id);
+                ps.executeUpdate();
+                ResultSet ids = ps.getGeneratedKeys();
+                ids.next();
+                int bmiEntryId = ids.getInt(1);
+
+                //todo: her skal vi indsætte hobbyer i link_hobby_bmi_entry tabellen
+                for (Integer hobbyID : hobbyList) {
+                    insertIntoLinkHobbyBmiEntry(bmiEntryId, (int)hobbyID);
+                }
+            }
+            catch (SQLException ex)
+            {
+                throw new UserException(ex.getMessage());
+            }
+        }
+        catch (SQLException ex)
+        {
+            throw new UserException(ex.getMessage());
+        }
+    }
+
+    public void insertIntoLinkHobbyBmiEntry(int bmiEntryID, int hobbyID) throws UserException{
+
+        try (Connection connection = database.connect())
+        {
+            String sql =
+                    "INSERT INTO link_bmi_hobby " +
+                            "(`hobby_id`, " +
+                            "`bmi_entry_id`) " +
+                            "VALUES (?, ?);";
+
+            try (PreparedStatement ps = connection.prepareStatement(sql))
+            {
+                ps.setInt(1, hobbyID);
+                ps.setInt(2, bmiEntryID);
+                ps.executeUpdate();
+            }
+            catch (SQLException ex)
+            {
+                throw new UserException(ex.getMessage());
+            }
+        }
+        catch (SQLException ex)
+        {
+            throw new UserException(ex.getMessage());
+        }
+    }
+
+
+}
